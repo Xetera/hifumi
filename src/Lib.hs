256a64470171f793a5f8e4599b4b569b6a56ee80
Xetera <mythologyali@gmail.com>
2019-03-01 00:46:46 -0800

    added datadog redirect

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,10 +11,9 @@ import           Control.Lens
 import           Control.Monad.IO.Class
 import           Data.Aeson                           (ToJSON, Value)
 import           Data.Aeson.Lens
-import qualified Data.ByteString.Lazy.Char8           as L8
+import System.Environment
 import           Data.Maybe
-import qualified Data.Text                            as T
-import           Debug.Trace
+import Data.Text.Internal.Lazy as T
 import           GHC.Generics
 import           Network.HTTP.Simple
 import           Network.Wai.Middleware.RequestLogger (logStdout)
@@ -27,14 +26,20 @@ data StatsResponse = StatsResponse
 
 instance ToJSON StatsResponse
 
+getBody :: Response Value -> Value
 getBody res = getResponseBody res :: Value
 
+newGameUrl :: Request
 newGameUrl = "https://reddit.com/r/newgame/about.json"
 
+discordUrl :: Request
 discordUrl = "https://discordapp.com/api/v6/invite/ZWW5CJw?with_counts=true"
 
 fetchData :: Request -> (Value -> Maybe Int) -> IO (Maybe Int)
 fetchData url fn = fn . getBody <$> httpJSON url
+
+datadogUrl :: Text
+datadogUrl = "https://p.datadoghq.com/sb/71d9e3d68-233c63b5d43908deb0df73c63059cdb2"
 
 getNewGameSubs :: IO (Maybe Int)
 getNewGameSubs = fetchData newGameUrl extractSubs
@@ -56,11 +61,10 @@ getStatsJSON = convertJson <$> getStats
     convertJson (reddit, discord) = StatsResponse {discord = getDefault discord, reddit = getDefault reddit}
 
 start :: IO ()
-start = do
-  let port = 3000
-  putStrLn $ "Starting server on port" <> show port
+start = read <$> getEnv "NEWGAME_PORT" >>= \port ->
   scotty port $ do
     middleware logStdout
-    get "/api/stats" $ liftIO getStatsJSON >>= json
-    get "/api/discord" $ liftIO getDiscordMembers >>= json
-    get "/api/reddit" $ liftIO getNewGameSubs >>= json
+    get "/bot/stats" $ redirect datadogUrl
+    get "/social/stats" $ liftIO getStatsJSON >>= json
+    get "/social/discord" $ liftIO getDiscordMembers >>= json
+    get "/social/reddit" $ liftIO getNewGameSubs >>= json
