5e39c940d29fd502758f42b27dd9fba4e107254c
Xetera <mythologyali@gmail.com>
2019-03-21 13:19:20 -0700

    added CI

--- src/Cache.hs
+++ src/Cache.hs
@@ -1,15 +1,16 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Cache
-  ( generateCache, withCache, StatCache
+  ( generateCache
+  , withCache
+  , StatCache
   ) where
 
 import qualified Data.Cache    as C
-import Data.Hashable
+import           Data.Hashable
 import           GHC.Int       (Int64)
 import           System.Clock
 
 type StatCache = C.Cache String (Maybe Int)
+
 cacheDuration :: Int64
 cacheDuration = 60 * 60
 
@@ -17,8 +18,6 @@ generateCache :: IO (C.Cache k v)
 generateCache = C.newCache . Just $ TimeSpec {sec = cacheDuration, nsec = 0}
 
 withCache :: (Eq k, Hashable k) => C.Cache k v -> k -> IO v -> IO v
-withCache cache key func = value >>= C.insert cache key >> value
-  where
-    value = C.lookup cache key >>= \case
-        Just existingVal -> return existingVal
-        Nothing -> func
+withCache cache key func =
+  let value = C.lookup cache key >>= maybe func return
+   in value >>= C.insert cache key >> value
