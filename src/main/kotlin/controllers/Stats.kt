b8a0620d29c0699768e987c6f897e02b61b6585a
Xetera <mythologyali@gmail.com>
2019-03-24 15:03:30 -0700

    more concise requests

--- src/main/kotlin/controllers/Stats.kt
+++ src/main/kotlin/controllers/Stats.kt
@@ -1,13 +1,15 @@
 package controllers
 
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.coroutines.awaitStringResponse
 import com.google.gson.Gson
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
-import java.net.URL
 
 const val discordURL = "https://discordapp.com/api/v6/invite/ZWW5CJw?with_counts=true"
 const val redditUrl = "https://reddit.com/r/newgame/about.json"
+const val USER_AGENT = "discord-api:/r/NewGame (by /u/Deathmeter)"
 
 data class RedditResponseData(val subscribers: Int)
 data class RedditResponse(val data: RedditResponseData)
@@ -16,14 +18,27 @@ data class StatResponse(val discord: Int, val reddit: Int)
 
 val gson = Gson()
 
-fun get(url: String) = URL(url).readText()
+suspend fun <T>get(url: String, json: Class<T>): T =
+    gson.fromJson(Fuel.get(url)
+        .header("User-Agent", USER_AGENT)
+        .awaitStringResponse().third, json)
 
-suspend fun getStats() = coroutineScope {
-    val discordReq = async { get(discordURL) }
-    val redditReq = async { get(redditUrl) }
-    val (discordRes, redditRes) = awaitAll(discordReq, redditReq)
-    val reddit = gson.fromJson(redditRes, RedditResponse::class.java)
-    val discord = gson.fromJson(discordRes, DiscordResponse::class.java)
-    val out = StatResponse(reddit.data.subscribers, discord.approximate_member_count)
+suspend fun getDiscordAsync() = coroutineScope {
+    async {
+        val discord = get(discordURL, DiscordResponse::class.java)
+        discord.approximate_member_count
+    }
+}
+
+suspend fun getRedditAsync() = coroutineScope {
+    async {
+        val reddit = get(redditUrl, RedditResponse::class.java)
+        reddit.data.subscribers
+    }
+}
+
+suspend fun getStats(): String = coroutineScope {
+    val (discord, reddit) = awaitAll(getDiscordAsync(), getRedditAsync())
+    val out = StatResponse(discord, reddit)
     gson.toJson(out)
 }
