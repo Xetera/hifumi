d65f6d9a34ff90af892d693f026ae95f59e58f6d
whamer100 <whammer100@aol.com>
2019-04-09 23:19:37 -0700

    previous commit uploaded the wrong file
    fixed now

--- src/commands/util/id.ts
+++ src/commands/util/id.ts
@@ -1,5 +1,6 @@
 import { Command } from "discord-akairo";
 import { Message, RichEmbedOptions } from "discord.js";
+import { isNullOrUndefined } from "util";
 
 enum Modes {
   NONE = -1,
@@ -83,18 +84,21 @@ export default class extends Command {
   }
 
   public deconstruct(id: string, mode: Modes, more: string) {
-    const epoch = 1420070400000;
+    const EPOCH = 1420070400000;
     if (/^\d{17,19}$/.test(id)) {
-      const PAD_START = 64;
-      const SLIVE_PID_INC = 52;
-      const SLICE_WID_PID = 47;
-      const SLICE_TS_WID = 42;
+      const PADDING = 64;
+      const INC_START = 52;
+      const PID_END = 52;
+      const PID_START = 47;
+      const WID_END = 47;
+      const WID_START = 42;
+      const TS_END = 42;
 
-      const bin = this.decToBin(id).padStart(PAD_START, "0");
-      const inc = parseInt(bin.slice(SLIVE_PID_INC), 2);
-      const pid = parseInt(bin.slice(SLICE_WID_PID, SLIVE_PID_INC), 2);
-      const wid = parseInt(bin.slice(SLICE_TS_WID, SLICE_WID_PID), 2);
-      const ts = parseInt(bin.slice(0, SLICE_TS_WID), 2) + epoch;
+      const bin = this.decToBin(id).padStart(PADDING, "0");
+      const inc = parseInt(bin.slice(INC_START), 2);
+      const pid = parseInt(bin.slice(PID_START, PID_END), 2);
+      const wid = parseInt(bin.slice(WID_START, WID_END), 2);
+      const ts = parseInt(bin.slice(0, TS_END), 2) + EPOCH;
       const date = new Date(ts).toUTCString();
 
       if (more === "true") {
@@ -102,6 +106,8 @@ export default class extends Command {
       } else {
         if (mode === Modes.MEMBER) {
           return `**Account Creation Date:**\n  _${date}_`;
+        } else if (mode === Modes.EMOTE || mode === Modes.ANIMATED_EMOTE) {
+          return `**Emote Creation Date:**\n_${date}_`;
         } else {
           return `**Timestamp:**\n_${date}_`;
         }
@@ -109,12 +115,16 @@ export default class extends Command {
     }
   }
 
-  public parser(id: any, mode: number, ctx: Message, more?: any, extra?: any) {
+  public parser(id: any, mode: number, ctx: Message, more?: any, extra?: string) {
     const error: RichEmbedOptions = {
       color: 0xFF0000,
       title: "Error",
       description: "Please submit a valid discord snowflake/mention."
     };
+
+    if (mode === Modes.NONE) {
+      return error;
+    }
 
     const hifumi = "https://cdn.discordapp.com/emojis/457982593028325376.png?v=1";
 
@@ -136,12 +146,13 @@ export default class extends Command {
       const ustr = [`Nickname: ${member.displayName}`, `Tag: ${user.tag}`, `Status: ${(user.bot) ? "Bot" : "User"}`];
 
       const ext = (more === "true") ? ` | ${id}` : "";
+      const ts = new Date(member.joinedTimestamp).toUTCString();
 
       const embed: RichEmbedOptions = {
         color: 0x7DD9FE,
         title: this.deconstruct(id, mode, more),
         thumbnail: { url: user.avatarURL, height: 32, width: 32 },
-        footer: { text: `Joined server at: ${new Date(member.joinedTimestamp).toUTCString()}${ext}`, icon_url: hifumi },
+        footer: { text: `Joined server at: ${ts}${ext}`, icon_url: hifumi },
         fields: [{
           name: `${user.username}'s Info`,
           value: `${ustr.join("\n")}`,
@@ -153,9 +164,71 @@ export default class extends Command {
         }]
       };
       return embed;
-    }
+    } else if (mode === Modes.EMOTE || mode === Modes.ANIMATED_EMOTE) {
+      const extension = (mode === Modes.ANIMATED_EMOTE) ? ".gif" : ".png";
+      const emoteURL = `https://cdn.discordapp.com/emojis/${id}${extension}`;
+      const ext = (more === "true") ? `| ${id}` : "";
+      const type = (mode === Modes.ANIMATED_EMOTE) ? "animated" : "static";
 
-    return error;
+      const ZWSP = "\u200B"; // to allow for blank fields
+      const data = [`${ZWSP} Name: ${extra}`, `${ZWSP} Type: ${type}`];
+
+      const embed: RichEmbedOptions = {
+        color: 0x7DD9FE,
+        title: this.deconstruct(id, mode, more),
+        thumbnail: { url: emoteURL, height: 32, width: 32 },
+        footer: { text: `${ZWSP}${ext}`, icon_url: hifumi },
+        fields: [{
+          name: "Emote Information",
+          value: data.join("\n")
+        }],
+        description: emoteURL
+      };
+      return embed;
+    } else if (mode === Modes.CHANNEL) {
+      const channel = ctx.guild.channels.get(id);
+      if (typeof channel === "undefined") {
+        const cError = error;
+        cError.description = "Please submit a valid discord channel from this guild.";
+        return cError;
+      }
+
+      const ext = (more === "true") ? `| ${id}` : "";
+
+      const cts = channel.createdAt.toUTCString();
+      const name = channel.name;
+      const pos = channel.calculatedPosition;
+      const category = channel.parent;
+
+      const hasCat = (isNullOrUndefined(category)) ? false : true;
+      const catName = (hasCat === true) ? category.name : "none";
+      const children = (hasCat === true) ? category.children : "none";
+      const catDat = (hasCat === true) ? [`Name: ${catName}`, `Children: ${children}`] : [];
+
+      const cstr = [`Name: ${name}`, `Position: ${pos}`, `Has Category: ${hasCat}`];
+
+      const embed: RichEmbedOptions = {
+        color: 0x7DD9FE,
+        title: this.deconstruct(id, mode, more),
+        footer: { text: `Channel created on ${cts}${ext}`, icon_url: hifumi },
+      };
+
+      if (hasCat) {
+        embed.fields = [{
+          name: "Channel Information:",
+          value: cstr.join("\n")
+        }, {
+          name : `${(hasCat) ? "Category Information:" : ""}`,
+          value: `${(hasCat) ? catDat.join("\n") : ""}`
+        }];
+      } else {
+        embed.fields = [{
+          name: "Channel Information:",
+          value: cstr.join("\n")
+        }];
+      }
+      return embed;
+    }
   }
 
   public exec(message: Message, { id, more, _ }: any) {
