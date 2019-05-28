9ea06b4e3e7dda9f6409c745b130de283e60d6d5
Xetera <mythologyali@gmail.com>
2019-05-27 21:50:08 -0700

    fixed a bug where users that share more than one guild wouldn't get saved more than once

--- src/db.ts
+++ src/db.ts
@@ -11,23 +11,25 @@ import { guilds_constraint, guilds_update_column } from "./graphql/schema";
 import { logger } from "./utils";
 
 const client = new GraphQLClient(GRAPHQL_ENDPOINT, {
-  headers: process.env.HASURA_ACCESS_KEY ? {
-    "X-Hasura-Access-Key": process.env.HASURA_ACCESS_KEY
-  } : {}
+  headers: process.env.HASURA_ACCESS_KEY
+    ? {
+        "X-Hasura-Access-Key": process.env.HASURA_ACCESS_KEY
+      }
+    : {}
 });
 
-export const syncAll = ({ guilds, users }: AkairoClient) => Promise.all([
-  syncGuilds(guilds.array()),
-  syncUsers(users.array()),
-  syncMembers(
-    new Collection<string, GuildMember>()
-      .concat(...guilds.map((guild) => guild.members))
-      .array()
-  )
-]);
+export const syncAll = ({ guilds, users }: AkairoClient) =>
+  Promise.all([
+    syncGuilds(guilds.array()),
+    syncUsers(users.array()),
+    syncMembers(
+      guilds.map(guild => guild.members.array())
+        .reduce((a, b) => a.concat(b))
+    )
+  ]);
 
 export const _client = createClient({
-  fetcher: ({ query, variables }: { query: any, variables: any }, fetch: any) =>
+  fetcher: ({ query, variables }: { query: any; variables: any }, fetch: any) =>
     fetch(process.env.HASURA_URL!, {
       method: "POST",
       body: JSON.stringify({ query, variables }),
@@ -42,40 +44,39 @@ export const req = (q: string, vars?: Variables) => client.request(q, vars);
 export const syncGuilds = (guilds: Guild[]) => {
   logger.info("Synchronizing guilds");
   return _client.mutation({
-    insert_guilds: [{
-      objects: guilds.map((guild) => ({
-        guild_id: guild.id,
-        name: guild.name,
-        icon: guild.iconURL
-      })),
-      on_conflict: {
-        constraint: guilds_constraint.guilds_pkey,
-        update_columns: [
-          guilds_update_column.name,
-          guilds_update_column.icon
-        ]
+    insert_guilds: [
+      {
+        objects: guilds.map(guild => ({
+          guild_id: guild.id,
+          name: guild.name,
+          icon: guild.iconURL
+        })),
+        on_conflict: {
+          constraint: guilds_constraint.guilds_pkey,
+          update_columns: [guilds_update_column.name, guilds_update_column.icon]
+        }
+      },
+      {
+        affected_rows: 1
       }
-    }, {
-      affected_rows: 1
-    }]
+    ]
   });
 };
 
 export const syncUsers = (users: User[]) => {
   logger.info("Synchronizing users");
-  const data = users.map((user): Users_Insert_Input => ({
-    user_id: user.id,
-    name: user.username,
-    avatar: user.displayAvatarURL
-  }));
+  const data = users.map(
+    (user): Users_Insert_Input => ({
+      user_id: user.id,
+      name: user.username,
+      avatar: user.displayAvatarURL
+    })
+  );
   const query = gql`
     mutation($data: [users_insert_input!]!) {
       insert_users(
         objects: $data
-        on_conflict: {
-          constraint: users_pkey
-          update_columns: [name, avatar]
-        }
+        on_conflict: { constraint: users_pkey, update_columns: [name, avatar] }
       ) {
         returning {
           user_id
@@ -89,13 +90,16 @@ export const syncUsers = (users: User[]) => {
 const syncMembers = (members: GuildMember[]) => {
   logger.info("Synchronizing members");
   return _client.mutation({
-    insert_members: [{
-      objects: members.map((member: GuildMember) => ({
-        guild_id: member.guild.id,
-        user_id: member.user.id,
-      })),
-    }, {
-      affected_rows: 1
-    }]
+    insert_members: [
+      {
+        objects: members.map((member: GuildMember) => ({
+          guild_id: member.guild.id,
+          user_id: member.user.id
+        }))
+      },
+      {
+        affected_rows: 1
+      }
+    ]
   });
 };
